16128efdc767a60d73b3619ce7f74409443d2ba2
Kit <kitsimcoe@gmail.com>
2023-04-19 22:45:06 +0100

    Added Choice buttons, Score counters and win conditionals

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,16 +1,82 @@
-let choices = ["Rock!", "Paper!", "Scissors!"]
 
+let choices = ["Rock", "Paper", "Scissors"]
 
+//gets Computer choice and returns it
 function getComputerChoice() {
-    // choose from array at random
-    let computerChoice = choices[(Math.floor(Math.random() * choices.length))];
-    return computerChoice;
-    
+    return (choices[(Math.floor(Math.random() * choices.length))]); //maths chooses from array at random 
 }
 
 
-console.log(getComputerChoice())
-console.log(choices.length)
+
+// function to count player wins
+let playerWins = 0
+
+function playerWinCounter() {
+ updatedisplay(++playerWins)
+
+    function updatedisplay(val) {
+    document.getElementById("playerWinCounter").innerHTML = val;
+    }
+}
+
+
+//function to count computer wins
+let computerWins = 0
+
+function computerWinCounter() {
+ updatedisplay(++computerWins)
+
+    function updatedisplay(val) {
+    document.getElementById("computerWinCounter").innerHTML = val;
+    }
+}
+
+
+
+function round(selection) {
+
+    // Gets Player selection via buttons on the html & computer choice via function
+    let playerChoice = selection.value;
+    let computerChoice = getComputerChoice();
+
+    alert("You have selected " + playerChoice);
+    alert("Computer has selected " + computerChoice);
+
+    if (computerChoice == "Rock" && playerChoice == "Paper") {
+        alert("You Win!");
+        playerWinCounter();
+
+    } else if (computerChoice == "Rock" && playerChoice == "Scissors") {
+        alert("You Lose!");
+        computerWinCounter();
+
+    } else if (computerChoice == "Paper" && playerChoice == "Scissors") {
+        alert("You Win!");
+        playerWinCounter();
+
+    } else if (computerChoice == "Paper" && playerChoice == "Rock") {
+        alert("You Lose!");
+        computerWinCounter();
+
+    } else if (computerChoice == "Scissors" && playerChoice == "Paper") {
+        alert("You Lose!");
+        computerWinCounter();
+
+    } else if (computerChoice == "Scissors" && playerChoice == "Rock") {
+        alert("You Win!");
+        playerWinCounter();
+
+    } else {
+        alert("Draw!")
+    }
+    
+    return (computerChoice);
+}
+
+
+
+
+
 
 
 
