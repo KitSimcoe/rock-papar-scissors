3817f2fa74ff6a274e8f3b197b9d4556777136ec
Kit <kitsimcoe@gmail.com>
2023-04-23 12:52:18 +0100

    moved script from html to js file. Refactored players choice. Added sound file.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,5 @@
 
-let choices = ["Rock", "Paper", "Scissors"]
+const choices = ["Rock", "Paper", "Scissors"]
 
 //gets Computer choice and returns it
 function getComputerChoice() {
@@ -7,34 +7,17 @@ function getComputerChoice() {
 }
 
 
+//adds event listener for inputs and returns players choice
+let button = document.querySelectorAll('input');
 
-// function to count player wins
-let playerWins = 0
-
-function playerWinCounter() {
- updatedisplay(++playerWins)
-
-    function updatedisplay(val) {
-    document.getElementById("playerWinCounter").innerHTML = val; // innerHTML edits the content of the element.
-    }
-}
+button.forEach((selection) => {
+    selection.addEventListener('click', (e) => {
+        console.log(e);
+        alert(selection.value)
+    });
+    });
 
 
-//function to count computer wins
-let computerWins = 0
-console.log(computerWins);
-
-
-function computerWinCounter() {
- updatedisplay(++computerWins)
- 
-
-    function updatedisplay(val) {
-    document.getElementById("computerWinCounter").innerHTML = val;
-    
-    
-    }
-}
 
 
 
@@ -43,25 +26,28 @@ function checkWinner() {
         alert("Congratulations! You beat the computer. The game will now reset.")
         document.getElementById("computerWinCounter").innerHTML = 0;
         document.getElementById("playerWinCounter").innerHTML = 0;
+        computerWins = 0
+        playerWins = 0
+        
     
     } else if (computerWins == 5) {
         alert ("You lost the game :( Better luck next time.")
         document.getElementById("computerWinCounter").innerHTML = 0;
         document.getElementById("playerWinCounter").innerHTML = 0;
+        computerWins = 0
+        playerWins = 0
    
     } else {
         return;
     }
 }
 
-function round(selection) {
+function round(playerChoice) {
 
     // Gets Player selection via buttons on the html & computer choice via function
-    let playerChoice = selection.value;
     let computerChoice = getComputerChoice();
 
-    alert("You have selected " + playerChoice);
-    alert("Computer has selected " + computerChoice);
+    alert(`You have selected ${playerChoice} Computer has selected ${computerChoice}`)
 
     if (computerChoice == "Rock" && playerChoice == "Paper") {
         alert("You win this round!");
@@ -100,6 +86,33 @@ function round(selection) {
 
 
 
+// function to count player wins
+let playerWins = 0
+
+function playerWinCounter() {
+ updatedisplay(++playerWins)
+
+    function updatedisplay(val) {
+    document.getElementById("playerWinCounter").innerText = val; // innerHTML edits the content of the element.
+    }
+}
+
+
+//function to count computer wins
+let computerWins = 0
+console.log(computerWins);
+
+
+function computerWinCounter() {
+ updatedisplay(++computerWins)
+ 
+
+    function updatedisplay(val) {
+    document.getElementById("computerWinCounter").innerHTML = val;
+    
+    
+    }
+}
 
 
 
